dae651c97b98d231aace1b2ca592086bba9107ab
Ilya Novozhilov <ilja.keinn@mail.ru>
2015-11-18 17:04:53 +0300

    Home Task 1

---
+++ MyString.java
@@ -0,0 +1,56 @@
+
+public class MyString {
+
+    public  static  int lengthOfString(String str, char symbol) {
+
+        if (str == null || str == "")
+            return -1;
+
+        int countSymbols = 0;
+
+        for (int i = 0; i < str.length(); ++i) {
+
+            if (str.charAt(i) == symbol) {
+                ++countSymbols;
+            }
+        }
+
+        return  countSymbols;
+    }
+
+    public static boolean searchSameElems (String str, String pattern) {
+
+        if (str.length() < pattern.length()) {
+            return false;
+        }
+
+        int indexMatches = 0;
+
+        boolean flagCoincidence = false;
+
+        for (int i = 0; i < str.length() - pattern.length(); ++i) {
+
+            flagCoincidence = true;
+
+            for (int j = i; j < pattern.length(); ++j) {
+
+                if (str.charAt(j) != pattern.charAt(j)) {
+                    flagCoincidence = false;
+                    break;
+                }
+            }
+
+            if (flagCoincidence) {
+                ++indexMatches;
+            }
+        }
+
+        if (indexMatches > 1) {
+            return true;
+        }
+
+        else {
+            return false;
+        }
+    }
+}
